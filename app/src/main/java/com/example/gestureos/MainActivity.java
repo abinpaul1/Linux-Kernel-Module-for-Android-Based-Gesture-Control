f6500b7c7e6ecc1d2a1d5d28323954425c88d4b2
abinpaul1 <abin.paul1@gmail.com>
2019-11-11 20:30:21 +0530

    Voice control added

--- app/src/main/java/com/example/gestureos/MainActivity.java
+++ app/src/main/java/com/example/gestureos/MainActivity.java
@@ -2,20 +2,44 @@ package com.example.gestureos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "OSP";
+    private final int REQ_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView speak = findViewById(R.id.speak);
+        speak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Need to Speak");
+                try{
+                    startActivityForResult(intent,REQ_CODE);
+                } catch (ActivityNotFoundException a){
+                        Toast.makeText(getApplicationContext(),"Sorry your device is not supported",Toast.LENGTH_SHORT).show();
+            }
+        }
+        });
     }
 
     public void check_and_log(View view){
@@ -40,5 +64,36 @@ public class MainActivity extends AppCompatActivity {
         Log.d(MainActivity.TAG,msg);
         String t = ".";
         Log.d(MainActivity.TAG, t);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        switch (requestCode){
+            case REQ_CODE: {
+                if (resultCode == RESULT_OK && null != data){
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+//                    Toast.makeText(getApplicationContext(),result.get(0),Toast.LENGTH_SHORT).show();
+                    String msg_text = result.get(0).toLowerCase();
+                    String msg="-1";
+                    switch (msg_text){
+                        case "lock":
+                            msg = "L";
+                            break;
+                        case "sleep":
+                            msg = "S";
+                            break;
+                        case "turn off":
+                            msg = "T";
+                            break;
+                    }
+                    Log.d(MainActivity.TAG,msg);
+                    String t = ".";
+                    Log.d(MainActivity.TAG, t);
+                }
+                break;
+            }
+        }
     }
 }
